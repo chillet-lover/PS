8f65b91933265cc4d3c85a6bc0c0ead7d9b95cae
potaroo <enohpolyx9310@naver.com>
2024-03-23 17:42:52 +0900

    백준 11280 2-SAT-3 AC

---
+++ AC/boj_11280.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <bitset>
+// #define DEBUG
+
+using namespace std;
+
+constexpr int MX = 10'000;
+vector<int> edges[MX * 2 + 1], trans[MX * 2 + 1];
+vector<vector<int>> sccs;
+stack<int> stk;
+bitset<MX * 2 + 1> visit;
+
+inline void normalize(int &vertex) {
+    if(vertex < 0) vertex = abs(vertex) + MX;  
+}
+
+inline int get_not(const int &vertex) {
+    return vertex > MX ? vertex - MX : vertex + MX;
+}
+
+void dfs(const int &vertex) {
+    visit[vertex] = true;
+    for(const int &next : edges[vertex]) {
+        if(visit[next]) continue;
+        dfs(next);
+    }
+    stk.push(vertex);
+}
+
+void make_scc(const int &vertex, vector<int> &scc) {
+    visit[vertex] = true;
+    scc.push_back(vertex);
+    for(const int &next : trans[vertex]) {
+        if(visit[next]) continue;
+        make_scc(next, scc);
+    }
+}
+
+int main() {
+    cin.tie(0);
+    cout.tie(0);
+    cin.sync_with_stdio(0);
+
+    register int i;
+    int N, M;
+
+    cin >> N >> M;
+    for(i = M; i; --i) {
+        int xi, xj, nxi, nxj;
+        cin >> xi >> xj;
+        normalize(xi), normalize(xj);
+        nxi = get_not(xi), nxj = get_not(xj);
+        edges[xi].push_back(nxj);
+        edges[xj].push_back(nxi);
+        trans[nxi].push_back(xj);
+        trans[nxj].push_back(xi);
+    }
+
+    for(i = 1; i <= N; ++i) {
+        if(!visit[i]) dfs(i);
+        if(!visit[i + MX]) dfs(i + MX);
+    }
+    visit.reset();
+    while(stk.size()) {
+        const int top = stk.top();
+        stk.pop();
+        if(!visit[top]) {
+            vector<int> scc;
+            make_scc(top, scc);
+            sccs.push_back(scc);
+            #ifdef DEBUG
+                cout << "scc group : " <<  sccs.size() << "\n";
+                for(const int &vertex : scc) {
+                    cout << vertex << " ";
+                }
+                cout << "\n";
+            #endif
+        }
+    }
+    for(const vector<int> &scc : sccs) {
+        visit.reset();
+        for(const int &vertex : scc) {
+            visit[vertex] = true;
+            if(visit[vertex] && visit[get_not(vertex)]) {
+                cout << "0\n";
+                return 0;
+            }
+        }
+    }
+    cout << "1\n";
+}
